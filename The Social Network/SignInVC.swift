b3ac583198a2774a5143578085d6d6a3e40dd16a
frarredo <frarredo@gmail.com>
2017-07-14 18:34:20 -0700

    installed Firebase modules and added assests

---
+++ The Social Network/SignInVC.swift
@@ -0,0 +1,18 @@
+//
+//  SignInVC.swift
+//  The Social Network
+//
+//  Created by Francisco on 7/14/17.
+//
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
