86f7abc649ee5af0a5b51f07bc45583c8d8c92a2
frarredo <frarredo@gmail.com>
2017-07-14 17:58:15 -0700

    Initial Commit

---
+++ The Social NetworkTests/The_Social_NetworkTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Social_NetworkTests.swift
+//  The Social NetworkTests
+//
+//  Created by Francisco on 7/14/17.
+//
+//
+
+import XCTest
+@testable import The_Social_Network
+
+class The_Social_NetworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
