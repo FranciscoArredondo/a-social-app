e23e224cea0895cd69d31fbcd0b7f8ca9d207231
frarredo <frarredo@gmail.com>
2017-07-17 21:15:01 -0700

    completed firebase post display data

--- The Social Network/PostCell.swift
+++ The Social Network/PostCell.swift
@@ -15,6 +15,8 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var postImage: UIImageView!
     @IBOutlet weak var caption: UITextView!
     @IBOutlet weak var likesLabel: UILabel!
+    
+    var post: Post!
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -22,6 +24,26 @@ class PostCell: UITableViewCell {
         postImage.roundCorners(cornerRadius: 3.0)
         
         // Initialization code
+    }
+    
+    func configureCell(post: Post) {
+        self.post = post
+        caption.text = post.caption
+        likesLabel.text = "\(post.likes)"
+        
+        // temporary for now...
+        let url = URL(string: post.imageUrl)!
+        
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+                DispatchQueue.global().sync {
+                    self.postImage.image = UIImage(data: data)
+                }
+            } catch {
+                // handel the error
+            }
+        }
     }
 
 }
